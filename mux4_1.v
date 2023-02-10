ab7e8ade864928c96e6becb5a9b43cbd91c0cd57
jayfurz <jfursov@uci.edu>
2023-02-09 18:41:27 -0800

    Update mux4_1.v

--- mux4_1.v
+++ mux4_1.v
@@ -1,6 +1,15 @@
+
+/**************************************************************************
+***									                                                    ***
+*** ECE 526 L Experiment #1 ***						                              ***
+*** Experiment #1 Familiarization with Linux and VCS ***		            ***
+*** ***********************************************************************
+*** Filename: mux4_1.v Created by Justin Fursov, Feb 09 2023 *** 	      ***
+*** ---   Initial Release  ---			 			                              ***
+***************************************************************************/
 `timescale 1 ns/ 1 ns
 module mux4_1(out, a, b, c, d, s0, s1);
-//port dedications
+// port dedications
 output out;
 input a, b, c, d, s0, s1;
 wire s0_n, s1_n, T1, T2, T3, T4;
