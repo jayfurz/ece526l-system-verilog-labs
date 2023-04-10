3c458ccbcd3539ae2243941a428f1c43935ef01d
Justin A Fursov <jaf39908@dcd145.csun.edu>
2023-04-09 23:15:25 -0700

    changed it so no explicit x case

--- lab6/mux.v
+++ lab6/mux.v
@@ -9,28 +9,19 @@
 
 /* This is the module for the variable size mux */
 
-`timescale 1ns/100ps
-
 module mux(out, a, b, sel);
 
-parameter Width = 1;
-input [Width-1:0] a, b;
-input sel;
-output reg [Width-1:0] out;
-reg [31:0] count;
+    parameter Width = 1;  // Default width of the multiplexer
+    input [Width-1:0] a, b;  // Input buses A and B
+    input sel;  // Selection signal
+    output [Width-1:0] out;  // Output bus
 
-always @ (a,b,sel) begin
-    count = 0;
-    while (count < Width) begin
-        if (a[count] == b[count]) begin
-            out[count] <= a[count];
-        end else if (~sel) begin
-            out[count] <= a[count];
-        end else if (sel) begin
-            out[count] <= b[count];
-        end else out[count] <= sel;
-        count = count + 1;
+    genvar i;
+    generate
+    for (i = 0; i < Width; i = i + 1) begin: bit_mux
+        assign out[i] = (a[i] == b[i])? a[i] : (a[i] & ~sel) | (b[i] & sel);
     end
-end
+    endgenerate
 
 endmodule
+
