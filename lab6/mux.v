c42dfc7f5f62d48e883430a370d274dff0fa6393
Justin A Fursov <jaf39908@dcd145.csun.edu>
2023-04-09 22:31:26 -0700

    finished lab6

--- lab6/mux.v
+++ lab6/mux.v
@@ -3,7 +3,7 @@
 *** ECE 526 L Lab #6                                                    ***
 *** Experiment #6 Variable Size Multiplexer Lab                         ***
 *** ***********************************************************************
-*** Filename: mux.v Created by Justin Fursov, Mar 29 2023              ***
+*** Filename: mux.v Created by Justin Fursov, Mar 29 2023               ***
 ***                                                                     ***
 ***************************************************************************/
 
@@ -16,7 +16,21 @@ module mux(out, a, b, sel);
 parameter Width = 1;
 input [Width-1:0] a, b;
 input sel;
-output [Width-1:0] out;
+output reg [Width-1:0] out;
+reg [31:0] count;
 
-assign    out = (sel? b: a); //  two bits are not the same and sel is x
+always @ (a,b,sel) begin
+    count = 0;
+    while (count < Width) begin
+        if (a[count] == b[count]) begin
+            out[count] <= a[count];
+        end else if (~sel) begin
+            out[count] <= a[count];
+        end else if (sel) begin
+            out[count] <= b[count];
+        end else out[count] <= sel;
+        count = count + 1;
+    end
+end
+
 endmodule
--- lab6/mux_tb.v
+++ lab6/mux_tb.v
@@ -3,34 +3,70 @@
 *** ECE 526 L Lab #6                                                    ***
 *** Experiment #6 Variable Size Multiplexer Lab                         ***
 *** ***********************************************************************
-*** Filename: mux_tb.v Created by Justin Fursov, Mar 29 2023              ***
+*** Filename: mux_tb.v Created by Justin Fursov, Mar 29 2023               ***
 ***                                                                     ***
 ***************************************************************************/
 
 /* This is the testbench for the variable size mux */
+`timescale 1ns / 1ps
 
-`timescale 1ns/100ps
+module testbench;
+reg [5:0] a, b;
+wire out1;
+wire [3:0] out4;
+wire [4:0] out5;
+wire [5:0] out6;
+reg sel;
 
-module mux_tb();
-reg a, b, sel;
-wire out;
-mux mux1(out, a, b, sel);
+mux mux1(
+    .out(out1),
+    .a(a[0:0]),
+    .b(b[0:0]),
+    .sel(sel));
+mux #(4) mux4(
+    .out(out4),
+    .a(a[3:0]),
+    .b(b[3:0]),
+    .sel(sel));
+mux #(5) mux5(
+    .out(out5),
+    .a(a[4:0]),
+    .b(b[4:0]),
+    .sel(sel));
+mux #(6) mux6(
+    .out(out6),
+    .a(a),
+    .b(b),
+    .sel(sel));
 
 initial begin
     $vcdpluson;
-    $display("This is the test of the one wide multiplexer");
-    //$monitor("%d: The output is: %b, the sel: %b, a: %b, b: %b", $time, out, sel, a, b);
-    $display("%d: The output is: %b, the sel: %b, a: %b, b: %b", $time, out, sel, a, b);
-    a = 1; b = 0;
+    $display("Testing all instances simultaneously");
+    a = 6'b100111; b = 6'b010000;
     sel = 0;
-    #1;
-    $display("%d: The output is: %b, the sel: %b, a: %b, b: %b", $time, out, sel, a, b);
+
+    #1 $display("Time: %d, sel: %b, a: %b, b: %b, out1: %b, out4: %b, out5: %b, out6: %b", $time, sel, a, b, out1, out4, out5, out6);
     #10 sel = 1;
-    #1;
-    $display("%d: The output is: %b, the sel: %b, a: %b, b: %b", $time, out, sel, a, b);
+    #1 $display("Time: %d, sel: %b, a: %b, b: %b, out1: %b, out4: %b, out5: %b, out6: %b", $time, sel, a, b, out1, out4, out5, out6);
     #10 sel = 0;
-    #1;
-    $display("%d: The output is: %b, the sel: %b, a: %b, b: %b", $time, out, sel, a, b);
+    #1 $display("Time: %d, sel: %b, a: %b, b: %b, out1: %b, out4: %b, out5: %b, out6: %b", $time, sel, a, b, out1, out4, out5, out6);
+
+    a = 6'b011101; b = 6'b101010;
+    #1 $display("Time: %d, sel: %b, a: %b, b: %b, out1: %b, out4: %b, out5: %b, out6: %b", $time, sel, a, b, out1, out4, out5, out6);
+    #10 sel = 1;
+    #1 $display("Time: %d, sel: %b, a: %b, b: %b, out1: %b, out4: %b, out5: %b, out6: %b", $time, sel, a, b, out1, out4, out5, out6);
+
+    // Test cases for sel = 1'bx
+    sel = 1'bx;
+         
+    // Case 1: A = B
+    a = 6'b110011; b = 6'b110011;
+    #1 $display("Time: %d, sel: %b, a: %b, b: %b, out1: %b, out4: %b, out5: %b, out6: %b", $time, sel, a, b, out1, out4, out5, out6);
+    
+    // Case 2: A != B
+    a = 6'b101101; b = 6'b010010;
+    #1 $display("Time: %d, sel: %b, a: %b, b: %b, out1: %b, out4: %b, out5: %b, out6: %b", $time, sel, a, b, out1, out4, out5, out6);
+
     $finish;
 end
 endmodule
