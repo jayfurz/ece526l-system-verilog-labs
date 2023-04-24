1966dbafc6b7d21a7f2348a6c6efcbce42939de2
Justin A Fursov <jaf39908@dcd145.csun.edu>
2023-04-24 01:58:28 -0700

    finished the lab

--- lab8/exhaustive_tb.v
+++ lab8/exhaustive_tb.v
@@ -39,6 +39,7 @@ always
 // Non-hierarchical sum of products with clocked data_in registers
 reg [DATA_WIDTH-1:0] data_in_reg1, data_in_reg2, data_in_reg3;
 reg [2*DATA_WIDTH+1:0] non_hierarchical_sum;
+reg [4:0] i, j, k, l, m;
 
 always @(posedge clk or posedge rst) begin
     if (rst) begin
@@ -56,11 +57,15 @@ always @(posedge clk) begin
     non_hierarchical_sum <= (data_in * coef11 + data_in_reg1 * coef12) + (data_in_reg2 * coef21 + data_in_reg3 * coef22);
 end
 
+// `define FORCE_ERROR
 `ifdef FORCE_ERROR
 initial begin
     $display("Forcing error at simulation time 1920");
-    #31457280 force sum_of_products_inst.final_sum = 1'b1;
+    #31457000 $monitoron;
+    #280 force sum_of_products_inst.final_sum = 1'b1;
+    #280 $monitoroff;
     #1940 release sum_of_products_inst.final_sum;
+
 end
 `endif
 
@@ -80,14 +85,14 @@ initial begin
     #10 rst = 0;
 
     // Monitor the inputs and output
-    $monitor("At time %t: data_in=%b coef11=%b coef12=%b coef21=%b coef22=%b final_sum=%b non_hierarchical_sum=%b", $time, data_in, coef11, coef12, coef21, coef22, final_sum, non_hierarchical_sum);
+    $monitor("At time %t: data_in=%d coef11=%d coef12=%d coef21=%d coef22=%d final_sum=%d non_hierarchical_sum=%d", $time, data_in, coef11, coef12, coef21, coef22, final_sum, non_hierarchical_sum);
     $monitoroff;
 
-    for (i = 0; i < 16; i++) begin
-        for (j = 0; j < 16; j++) begin
-            for (k = 0; k < 16; k++) begin
-                for (l = 0; l < 16; l++) begin
-                    for (m = 0; m < 16; m++) begin
+    for (i = 0; i < 16; i = i + 1) begin
+        for (j = 0; j < 16; j = j + 1) begin
+            for (k = 0; k < 16; k = k + 1) begin
+                for (l = 0; l < 16; l = l + 1) begin
+                    for (m = 0; m < 16; m = m + 1) begin
                         data_in <= i;
                         coef11 <= j;
                         coef12 <= k;
@@ -107,10 +112,13 @@ initial begin
                             $finish;
                         end
                         
-                        if (i < 2 || (i >= 14 && i <= 15)) begin
+                        // monitor on when it's either the first or last 16
+                        // vectors
+                        if ((i == 0 && j == 0 && k == 0 && l == 0) || (i == 15 && j == 15 && k == 15 && l == 15)) begin
                             $monitoron;
-                            $display("At time %t: data_in=%d coef11=%d coef12=%d coef21=%d coef22=%d final_sum=%d expected_sum=%d", $time, data_in, coef11, coef12, coef21, coef22, final_sum, non_hierarchical_sum);
-                        end else begin
+
+                        // monitor off after the first 16 vectors.
+                        end else if (i == 0 && j == 0 && k == 0 && l == 1 && m == 0) begin
                             $monitoroff;
                         end
                     end
--- lab8/lab8.f
+++ lab8/lab8.f
@@ -2,9 +2,11 @@
 
 if [ "$1" == "inex" ]; then
   vcs -full64 -debug level_1.v level_2.v top_level.v inexaustive_tb.v -lca -kdb -o simv_inex
-elif [ "$1" == "rom" ]; then
-  vcs -full64 -debug rom.v rom_tb.v -lca -kdb -o simv_rom
+elif [ "$1" == "ex" ]; then
+  vcs -full64 -debug level_1.v level_2.v top_level.v exhaustive_tb.v -lca -kdb -o simv_ex
+elif [ "$1" == "exerr" ]; then
+  vcs -full64 -debug -DFORCE_ERROR level_1.v level_2.v top_level.v exhaustive_tb.v -lca -kdb -o simv_exerr
 else
-  echo "Invalid argument. Please use either 'ram' or 'rom'."
+  echo "Invalid argument. Please use either 'inex' or 'ex'."
   exit 1
 fi
