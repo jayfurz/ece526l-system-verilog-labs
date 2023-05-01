f6126b9466e7c2f5a189a9353070a732f4078bc0
Justin Fursov <justinfursov@Justins-MacBook-Pro.local>
2023-04-30 22:40:06 -0700

    added edge cases

--- lab9/alu_tb.v
+++ lab9/alu_tb.v
@@ -21,6 +21,7 @@ module tb_alu;
     wire [7:0] alu_out;
     wire cf, of, sf, zf;
     reg [7:0] correct_answer;
+    reg [55:0] opcode_str;
 
     // Instantiate the ALU module
     alu uut (
@@ -45,14 +46,14 @@ module tb_alu;
         end
     endfunction
 
-    function automativ void check_result;
+    function automatic void check_result;
         input [55:0] opname;
         input [7:0] alu_out, expected_alu_out;
         begin
             if (alu_out === expected_alu_out) begin
-                $display("%0s test passed. Expected: %b, Got: %b", opname, expected_alu_out, alu_out);
+                $display("%0s test passed. Expected: %d, Got: %d", opname, expected_alu_out, alu_out);
             end else begin
-                $display("%0s test failed. Expected: %b, Got: %b", opnmae, expected_alu_out, alu_out);
+                $display("%0s test failed. Expected: %d, Got: %d", opnmae, expected_alu_out, alu_out);
             end
         end
     endfunction
@@ -85,7 +86,64 @@ module tb_alu;
         opcode = 4'b0110; a = 8'b1010_1010; b = 8'b0101_0101; correct_answer = a ^ b; #10;  // Test XOR
         opcode = 4'b0111; a = 8'b1111_0000; b = 8'b0000_0000; correct_answer = ~a; #10;  // Test NOT_A
 
-        // Additional test cases can be added here, following the test scenarios and corner cases discussed earlier.
+        $endmonitor;
+        // Additional test cases for corner cases discussed earlier.
+
+        // Test    // Test NOT A corner case: ~0
+        opcode = 4'b0111;
+        a = 8'b0000_0000;
+        b = 8'b0000_0000;
+        #10; opcode_str = opcode_name(opcode);
+        check_result(opcode_str, alu_out, 8'b1111_1111);
+
+        // Test ADD edge case: Maximum unsigned addition (255 + 255)
+        opcode = 4'b0010;
+        a = 8'b1111_1111;
+        b = 8'b1111_1111;
+        #10; opcode_str = opcode_name(opcode);
+        check_result(opcode_str, alu_out, 8'b1111_1110); // 254, with CF = 1
+
+        // Test SUB edge case: Maximum positive difference (255 - 0)
+        opcode = 4'b0011;
+        a = 8'b1111_1111;
+        b = 8'b0000_0000;
+        #10; opcode_str = opcode_name(opcode);
+        check_result(opcode_str, alu_out, 8'b1111_1111);
+
+        // Test SUB edge case: Maximum negative difference (0 - 255)
+        opcode = 4'b0011;
+        a = 8'b0000_0000;
+        b = 8'b1111_1111;
+        #10; opcode_str = opcode_name(opcode);
+        check_result(opcode_str, alu_out, 8'b0000_0001); // 1, with CF = 1
+
+        // Test AND edge case: AND with all 1s
+        opcode = 4'b0100;
+        a = 8'b1111_1111;
+        b = 8'b1111_1111;
+        #10; opcode_str = opcode_name(opcode);
+        check_result(opcode_str, alu_out, 8'b1111_1111);
+
+        // Test OR edge case: OR with all 0s
+        opcode = 4'b0101;
+        a = 8'b0000_0000;
+        b = 8'b0000_0000;
+        #10; opcode_str = opcode_name(opcode);
+        check_result(opcode_str, alu_out, 8'b0000_0000);
+
+        // Test XOR edge case: XOR with inverse values
+        opcode = 4'b0110;
+        a = 8'b1010_1010;
+        b = 8'b0101_0101;
+        #10; opcode_str = opcode_name(opcode);
+        check_result(opcode_str, alu_out, 8'b1111_1111);
+
+        // Test XOR edge case: XOR with the same values
+        opcode = 4'b0110;
+        a = 8'b1010_1010;
+        b = 8'b1010_1010;
+        #10; opcode_str = opcode_name(opcode);
+        check_result(opcode_str, alu_out, 8'b0000_0000);
 
         // Finish the simulation
         $finish;
